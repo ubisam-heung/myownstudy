0dc1dc4ac31e3b3b1b57a60cdb3a7bb7911350f3
DESKTOP-9OHE0FP\user <ncj>
2026-02-03 16:23:09 +0900

    20260203-학습용 myexample1 생성

---
+++ Semina/myexample1/src/test/java/com/ubisam/example1/Example1ApplicationTests.java
@@ -0,0 +1,40 @@
+package com.ubisam.example1;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Example1ApplicationTests {
+
+	@Autowired
+	private HelloRepository helloRepository;
+
+	@Test
+	void contextLoads() {
+		Hello h = new Hello();
+		h.setName("name1");
+		h.setEmail("abc@ubisam.com");
+
+		h = helloRepository.save(h);
+
+		// Read
+		Optional<Hello> h2 = helloRepository.findById(1l);
+		System.out.println(h.getId());
+		System.out.println(h2.get().getId());
+
+		// Update
+		h.setName("name2");
+		h = helloRepository.save(h);
+
+		// Search
+		List<Hello> r = helloRepository.findAll();
+
+		// Delete
+		helloRepository.delete(h);
+	}
+
+}
