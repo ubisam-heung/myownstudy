df1f0137e77b954d75bb66307cd01652ed413a94
choheungjae <heung@ubisam.com>
2026-02-03 15:36:49 +0900

    20260203-1차 세미나

---
+++ Semina/example1/src/main/java/com/ubisam/example1/Hello.java
@@ -0,0 +1,32 @@
+package com.ubisam.example1;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+//ORM - Object Relation Mapping
+@Entity
+@Data
+@Table(name = "t_str") //테이블 생성
+public class Hello {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    // @EmbeddedId
+    // private Id id;
+    private String name;
+    private String email;
+
+    // @Data
+    // @Embeddable
+    // public class Id{
+    //     private String id1;
+    //     private String id2;
+    // }
+
+
+}
---
+++ Semina/example1/src/main/java/com/ubisam/example1/HelloRepository.java
@@ -0,0 +1,12 @@
+package com.ubisam.example1;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
+public interface HelloRepository extends JpaRepository<Hello, Long>{
+    List<Hello> findByEmail(String email);
+    List<Hello> findByNameAndEmail(String name, String email);
+    List<Hello> findByIdOrName(Long id, String name);
+}
