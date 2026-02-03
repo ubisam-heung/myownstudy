6548c8b5a0476465cd1770f03d4e1cf9089b2c23
DESKTOP-9OHE0FP\user <ncj>
2026-02-03 17:12:34 +0900

    20260203-폴더 수정

---
+++ SeminaStudy/example1/src/main/java/com/ubisam/example1/HelloInsertExample.java
@@ -0,0 +1,20 @@
+package main.java.com.ubisam.example1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import jakarta.transaction.Transactional;
+
+@Component
+public class HelloInsertExample {
+
+    @Autowired
+    private HelloRepository helloRepository;
+
+    @Transactional
+    public void insertHello() {
+        Hello hello = new Hello();
+        hello.setName("김길동");
+        hello.setEmail("abc@naver.com");
+        helloRepository.save(hello);
+    }
+}
